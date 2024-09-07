0a323f26b40b523c87e2f15f606841ca832ad557
kaersie <113342527+Kaersie@users.noreply.github.com>
2024-09-07 13:09:57 +0000

    环境搭建

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+const app = createApp(App)
+
+createApp(App).use(ElementPlus).mount('#app')
